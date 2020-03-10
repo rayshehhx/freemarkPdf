2c375d6e902a4ce874300c8f92756fecffa9ed13
15021955235@163.com <15021955235@163.com>
2020-03-10 15:38:19 +0800

    测试页

--- src/main/java/com/demo/yao/pdfdemo/controller/PdfController.java
+++ src/main/java/com/demo/yao/pdfdemo/controller/PdfController.java
@@ -81,4 +81,59 @@ public class PdfController {
             }
         }
     }
+
+
+
+    @RequestMapping(value = "/export1",method = RequestMethod.GET)
+    public void exportPdf1(HttpServletResponse response) throws Exception {
+        ByteArrayOutputStream baos = null;
+        OutputStream out = null;
+        String s="你院（20XX）X法执X字第XX号执行裁定书，（20XX）X法X字第XX号协助执行通知书收悉，我行处理结果如下：" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分a。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分b。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分c。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分d。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分e。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分f。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分g。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分h。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分i。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分j。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分k。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分l。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分m。" +
+                "被执行人（或其他法律地位）XXX在我行XXXX账户内的存款已被冻结，已冻结可用金额人民币XX元，额度冻结金额人民币XX元。冻结期限自X年X月X日X时X分至X年X月X日X时X分n。" +
+                "......\n" +
+                "上述账户中，被执行人 XXX的XXXX账户为 xxx账户，权利人 XXX对账户中的XX(存款/资产)XXX元享有XXX权（权利类型）.\n" +
+                "....\n" +
+                "因XX原因，被执行人XXX在我行XXXX账户内的存款未能冻结。\n";
+        try {
+            // 模板中的数据，实际运用从数据库中查询
+            Map<String,Object> data = new HashMap<>();
+            data.put("bankName", "河南省金水区");
+            data.put("text", s);
+            data.put("date", new Date());
+
+            baos = PDFTemplateUtil.createPDF(data, "/ceshi.ftl");
+            // 设置响应消息头，告诉浏览器当前响应是一个下载文件
+            response.setContentType("application/x-msdownload");
+            // 告诉浏览器，当前响应数据要求用户干预保存到文件中，以及文件名是什么 如果文件名有中文，必须URL编码
+            String fileName = URLEncoder.encode("通知书.pdf", "UTF-8");
+            response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+            out = response.getOutputStream();
+            baos.writeTo(out);
+            baos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("导出失败：" + e.getMessage());
+        } finally {
+            if (baos != null) {
+                baos.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
 }
---
+++ src/main/resources/templates/ceshi.ftl
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>通知书</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            font-family: SimSun;
+        }
+
+        section {
+            display: block;
+            margin: 20px 10px;
+        }
+
+        .title {
+            text-align: center;
+        }
+
+        .preface p {
+            line-height: 30px;
+        }
+
+        .preface p.content {
+            text-indent: 2em;
+        }
+
+        /*div.header {*/
+        /*display: block;*/
+        /*text-align: center;*/
+        /*position: running(header);*/
+        /*}*/
+
+        div.footer {
+            display: block;
+            text-align: center;
+            position: running(footer);
+        }
+
+        @page {
+            @top-center {
+                content: element(header)
+            }
+        }
+
+        @page {
+            @bottom-center {
+                content: element(footer)
+            }
+        }
+
+        .pageNext {
+            page-break-after: always;
+        }
+
+        #pagenumber:before {
+            content: counter(page);
+        }
+
+        #pagecount:before {
+            content: counter(pages);
+        }
+
+        .pageNext {
+            page-break-after: always;
+        }
+
+        #pagenumber:before {
+            content: counter(page);
+        }
+
+        #pagecount:before {
+            content: counter(pages);
+        }
+    </style>
+</head>
+<body>
+<div class="footer">第 <span id="pagenumber"/> / <span id="pagecount"/> 页</div>
+<#--<div class="pageNext"></div>-->
+
+<div class="pageNext">
+    <!-- 标题 start -->
+    <section class="title">
+        <h2>${bankName}人民法院</h2>
+        <h2>协助执行通知书</h2>
+        <h2>（回执）</h2>
+    </section>
+    <!-- 标题 end -->
+
+    <!-- 前言 start -->
+    <section class="preface">
+        <p>${bankName}人民法院：</p>
+        <p class="content">${text}</p>
+    </section>
+    <!-- 前言 end -->
+
+    <!-- 汇总统计信息 start -->
+    <#--<section class="count-info">-->
+        <#--<p>${bankName}人民法院：</p>-->
+        <#--<p class="content">${text}</p>-->
+    <#--</section>-->
+    <!-- 汇总统计信息 end -->
+
+    <div align="right">
+        ${date?string("yyyy年MM月dd日HH时mm分ss秒")}
+        <br/>
+        <br/>
+        <img src="images/yinzhang.png" width="20%"/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+    </div>
+
+</div>
+</body>
+</html>
